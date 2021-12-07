e4219e72dcc8a5b19a22ac8760d36c3e1b04e376
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2021-12-07 12:11:19 -0600

    Improved themes further, added background to sections

--- js/index.js
+++ js/index.js
@@ -48,16 +48,20 @@ function closeModal(){
 function setTheme(theme) {
     let root = document.documentElement;
     if (theme === 0) {
-        root.style.setProperty('--primary-color', '#fff');
-        root.style.setProperty('--secondary-color', '#252525');
+
+        root.style.setProperty('--primary-color', '#f5f5fa');
+        root.style.setProperty('--text-color', '#000');
+        root.style.setProperty('--secondary-color', '#b6b7d5');
         root.style.setProperty('--accent-color', '#4E9F3D'); 
         
         localStorage.setItem("darkmode", "0")
 
         themeBtn.textContent = `brightness_7`
     } else if (theme === 1) {
+
         root.style.setProperty('--primary-color', '#252525');
-        root.style.setProperty('--secondary-color', '#fff');
+        root.style.setProperty('--text-color', '#fff');
+        root.style.setProperty('--secondary-color', '#121212');
         root.style.setProperty('--accent-color', '#5fc44b'); 
         
         localStorage.setItem("darkmode", "1")
