541ac1756c540b498615b679330657432c978ee1
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2022-02-22 22:28:47 -0600

    Added reposition input-field, improved result-box on payment page

--- js/payment.js
+++ js/payment.js
@@ -2,19 +2,28 @@ const baseSalaryEl = document.getElementById("base-salary-el")
 const bonusEl = document.getElementById("bonus-el")
 const notWorkedHoursEl = document.getElementById("not-worked-hours-el")
 const holidayBonusEl = document.getElementById("holiday-bonus-el")
+const repositionEl = document.getElementById("reposition-el")
 const nightHoursEl = document.getElementById("night-hours-el")
 const dominicalEl = document.getElementById("dominical-el")
 
 const paymentCalcBtn = document.getElementById("payment-calculate")
-const paymentResultEl = document.getElementById("payment-result")
+const totalEarnedEl = document.getElementById("total-earned-el")
+const totalNotWorkedEl = document.getElementById("total-not-worked-el")
+const totalPaidEl = document.getElementById("total-paid-el")
+
 const moneyPerHour = 30.38
+const nightHour = 10.53
 let baseSalary = 0
 let bonus = 0
 let notWorkedHours = 0
 let holidayBonus = 0
 let nightHours = 0
+let reposition = 0
 let dominicalBonus = 0
 let paymentResult = 0
+let totalEarned = 0
+let totalNotWorked = 0
+let totalPaid = 0
 
 function resetValues(){
     baseSalary = 0
@@ -27,62 +36,70 @@ function resetValues(){
 }
 
 paymentCalcBtn.addEventListener("click", function(){
-    paymentResultEl.innerHTML = " "
-
     if (notWorkedHoursEl.valueAsNumber){
         notWorkedHours = moneyPerHour * notWorkedHoursEl.valueAsNumber
         baseSalary = ((baseSalaryEl.valueAsNumber * 2) * (moneyPerHour))
-        bonus = (baseSalary - notWorkedHours) * (bonusEl.valueAsNumber / 100)
+        reposition = moneyPerHour * repositionEl.valueAsNumber
+        bonus = ((baseSalary - notWorkedHours) + reposition) * (bonusEl.valueAsNumber / 100)
 
         if (holidayBonusEl.valueAsNumber){
-            holidayBonus = (moneyPerHour * holidayBonusEl.valueAsNumber) + (moneyPerHour * holidayBonusEl.valueAsNumber) * 2
+            holidayBonus = (moneyPerHour * holidayBonusEl.valueAsNumber) * 2
         } else {holidayBonus === 0}
 
         if (dominicalEl.valueAsNumber){
         dominicalBonus = (moneyPerHour * dominicalEl.valueAsNumber) * .25
         } else {dominicalBonus === 0}
 
-        //Fix hard-coded value pending
         if (nightHoursEl.valueAsNumber){
-        nightHours = (10.53 * nightHoursEl.valueAsNumber)
+        nightHours = (nightHour * nightHoursEl.valueAsNumber)
         } else {nightHours === 0}
         
-        paymentResult = baseSalary + bonus + holidayBonus + dominicalBonus + nightHours - notWorkedHours
+        totalEarned = baseSalary + bonus + holidayBonus + dominicalBonus + nightHours + reposition
+        totalNotWorked = notWorkedHours
+        totalPaid = totalEarned - totalNotWorked
+
     } else {
         baseSalary = ((baseSalaryEl.valueAsNumber * 2) * (moneyPerHour))
-        bonus = baseSalary * (bonusEl.valueAsNumber / 100)
+        bonus = (baseSalary + reposition) * (bonusEl.valueAsNumber / 100)
 
         if (holidayBonusEl.valueAsNumber){
-            holidayBonus = (moneyPerHour * holidayBonusEl.valueAsNumber) + (moneyPerHour * holidayBonusEl.valueAsNumber) * 2
+            holidayBonus = (moneyPerHour * holidayBonusEl.valueAsNumber) * 2
         } else {holidayBonus === 0}
         
         if (dominicalEl.valueAsNumber){
             dominicalBonus = (moneyPerHour * dominicalEl.valueAsNumber) * .25
             } else {dominicalBonus === 0}
 
-        //Fix hard-coded value pending
         if (nightHoursEl.valueAsNumber){
-            nightHours = (10.53 * nightHoursEl.valueAsNumber)
+            nightHours = (nightHour * nightHoursEl.valueAsNumber)
             } else {nightHours === 0}
 
-
-        paymentResult = baseSalary + bonus + holidayBonus + dominicalBonus + nightHours
+        
+        totalEarned = baseSalary + bonus + holidayBonus + dominicalBonus + nightHours + reposition
+        totalNotWorked = notWorkedHours
+        totalPaid = totalEarned - totalNotWorked
     }
     
+    // paymentResultEl.innerHTML = `Approximately 
+    // <span class="text-accent">${paymentResult}$ MXN</span>
+    // (Mexican Pesos).`
 
-    // notWorkedHours = 30.38 * notWorkedHoursEl.valueAsNumber
-    // baseSalary = 30.38 * (baseSalaryEl.valueAsNumber * 2) - notWorkedHours
-    // bonus = baseSalary * (bonusEl.valueAsNumber / 100)
+    totalEarnedEl.textContent = `$ ${totalEarned}`
+    totalNotWorkedEl.textContent = `$ ${totalNotWorked}`
+    totalPaidEl.textContent = `$ ${totalPaid}`
     
-    // if (notWorkedHoursEl.valueAsNumber){  
-    //     paymentResult = Math.round(baseSalary + bonus - notWorkedHours)
-    // } else {
-    //     paymentResult = Math.round(baseSalary + bonus)
-    // }
-    
+    console.log(`Base salary: ${baseSalary}`)
+    console.log(`Reposition: ${reposition}`)
+    console.log(`Bonus: ${bonus}`)
+    console.log(`Holiday bonus: ${holidayBonus}`)
+    console.log(`Dominical bonus: ${dominicalBonus}`)
+    console.log(`Night hours: ${nightHours}`)
+    console.log(`Not worked hours: ${notWorkedHours}`)
+    console.log(`Total payment: ${paymentResult}`)
 
-    paymentResultEl.innerHTML = `Approximately 
-    <span class="text-accent">${paymentResult}$ MXN</span>
-    (Mexican Pesos).`
     resetValues()
+
 })
+
+
+
